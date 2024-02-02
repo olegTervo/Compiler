e5af17b17ae193d51fe41d07549965d768b91346
olegTervo <olegtervo@icloud.com>
2024-02-02 16:03:07 +0200

    improvements in parser

--- src/compiler/parser.py
+++ src/compiler/parser.py
@@ -20,6 +20,12 @@ class BinaryOp(Expression):
     op: str
     right: Expression
 
+@dataclass
+class IfExpression(Expression):
+    cond: Expression
+    then_clause: Expression
+    else_clause: Expression | None
+
 def parse(tokens: list[Token]) -> Expression:
     pos = 0
 
@@ -29,8 +35,10 @@ def parse(tokens: list[Token]) -> Expression:
         else:
             return Token(type='end', text='')
         
-    def consume() -> Token:
+    def consume(expected: str | None = None) -> Token:
         token = peek()
+        if expected is not None and token.text != expected:
+            raise Exception(f'Expected "{expected}", got "{token.text}"')
         nonlocal pos
         pos += 1
         return token
@@ -42,15 +50,59 @@ def parse(tokens: list[Token]) -> Expression:
             return Literal(value=int(token.text)) # TODO: error handling
         else:
             raise Exception(f'Expected literal, but found {token.text}') # TODO: row, col
-    
-    # TODO: left and right other than literal
+        
+    # TODO: reduce copy-paste
     def parse_expression() -> Expression:
-        left = parse_literal()
-        op_token = consume() # TODO: parse_token()
-        if op_token.text not in ['+', '-', '*', '/']:
-            raise Exception(f'Expected operator, got "{op_token.text}"')
-        right = parse_literal()
-        return BinaryOp(left, op_token.text, right)
+        left: Expression = parse_polynomial()
+        while peek().text in ['<', '>', '==', '>=', '<=']:
+            op_token = consume() # TODO: parse_token()
+            right = parse_polynomial()
+            left = BinaryOp(left, op_token.text, right)
+        return left
     
+    def parse_polynomial() -> Expression:
+        left: Expression = parse_term()
+        while peek().text in ['+', '-']:
+            op_token = consume() # TODO: parse_token()
+            right = parse_term()
+            left = BinaryOp(left, op_token.text, right)
+        return left
+    
+    def parse_term() -> Expression:
+        left: Expression = parse_factor()
+        while peek().text in ['*', '/']:
+            op_token = consume() # TODO: parse_token()
+            right = parse_factor()
+            left = BinaryOp(left, op_token.text, right)
+        return left
+    
+    def parse_factor() -> Expression:
+        if peek().text == '(':
+            return parse_parenthesized_expression()
+        elif peek().text == 'if':
+            return parse_if_expression()
+        elif peek().type == 'int_literal':
+            return parse_literal()
+        else:
+            raise Exception(f'Unexpected "{peek().text}"')
+        
+    def parse_parenthesized_expression() -> Expression:
+        consume('(')
+        expr = parse_expression()
+        consume(')')
+        return expr
+    
+    def parse_if_expression() -> Expression:
+        consume('if')
+        cond = parse_expression()
+        consume('then')
+        then_clause = parse_expression()
+        if peek().text == 'else':
+            consume('else')
+            else_clause = parse_expression()
+        else:
+            else_clause = None
+        return IfExpression(cond, then_clause, else_clause)        
+
     return parse_expression()
 
--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,11 +1,135 @@
 
-from compiler.parser import BinaryOp, Literal, parse
+from compiler.parser import BinaryOp, IfExpression, Literal, parse
 from compiler.tokenizer import tokenize
 
+def test_parser_can_parse() -> None:
+    assert parse(tokenize("1")) == Literal(1)
 
-def test_parser() -> None:
+def test_parser_simple_addition() -> None:
     assert parse(tokenize("1 + 2")) == BinaryOp(
         left= Literal(1),
-        op="+",
+        op='+',
         right = Literal(2)
+    )
+
+def test_parser_complex_addition() -> None:
+    assert parse(tokenize("1 + 2 + 3")) == BinaryOp(
+        left = BinaryOp(
+            left=Literal(1),
+            op='+',
+            right=Literal(2)
+        ),
+        op='+',
+        right=Literal(3)
+    )
+
+def test_parser_different_operators() -> None:
+    assert parse(tokenize("1 + 2 - 3")) == BinaryOp(
+        left = BinaryOp(
+            left=Literal(1),
+            op='+',
+            right=Literal(2)
+        ),
+        op='-',
+        right=Literal(3)
+    )
+
+def test_parser_right_multiplication() -> None:
+    assert parse(tokenize("1 + 2 * 3")) == BinaryOp(
+        left=Literal(1),
+        op='+',
+        right=BinaryOp(
+            left=Literal(2),
+            op='*',
+            right=Literal(3)
+        ),
+    )
+
+def test_parser_right_and_left_multiplication() -> None:
+    assert parse(tokenize("1 * 2 + 2 * 3")) == BinaryOp(
+        left=BinaryOp(
+            left=Literal(1),
+            op='*',
+            right=Literal(2)
+        ),
+        op='+',
+        right=BinaryOp(
+            left=Literal(2),
+            op='*',
+            right=Literal(3)
+        ),
+    )
+
+def test_parser_parenthesis() -> None:
+    assert parse(tokenize("(2+2)*2")) == BinaryOp(
+        left=BinaryOp(
+            left=Literal(2),
+            op='+',
+            right=Literal(2)
+        ),
+        op='*',
+        right=Literal(2)
+    )
+
+def test_parser_parenthesis_right() -> None:
+    assert parse(tokenize("2 * ( 2 + 2 )")) == BinaryOp(
+        left=Literal(2),
+        op='*',
+        right=BinaryOp(
+            left=Literal(2),
+            op='+',
+            right=Literal(2)
+        )
+    )
+
+def test_parser_complex_expression() -> None:
+    assert parse(tokenize("2 * ( 1 + 3 ) / 4")) == BinaryOp(
+        left=BinaryOp(
+            left=Literal(2),
+            op='*',
+            right=BinaryOp(
+                left=Literal(1),
+                op='+',
+                right=Literal(3)
+            )
+        ),
+        op='/',
+        right=Literal(4)
+    )
+
+def test_parser_comparison() -> None:
+    assert parse(tokenize('1 > 2+1')) == BinaryOp(
+        left=Literal(1),
+        op='>',
+        right=BinaryOp(
+            left=Literal(2),
+            op='+',
+            right=Literal(1)
+        )
+    )
+
+def test_parser_simple_if() -> None:
+    assert parse(tokenize('if 1 then 2')) == IfExpression(
+        cond=Literal(1),
+        then_clause=Literal(2),
+        else_clause=None
+    )
+
+def test_parser_if_with_expressions() -> None:
+    assert parse(tokenize('if 1 + 2 then 2 * 2 else 3 / 3')) == IfExpression(
+        cond=BinaryOp(
+            left=Literal(1),
+            op='+',
+            right=Literal(2)
+        ),
+        then_clause=BinaryOp(
+            left=Literal(2),
+            op='*',
+            right=Literal(2)
+        ),
+        else_clause=BinaryOp(
+            left=Literal(3),
+            op='/',
+            right=Literal(3)
+        )
     )
