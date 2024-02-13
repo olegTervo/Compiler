68c2f4d312cf0de0e750f3fd9f5c9a5494b5acae
olegTervo <olegtervo@icloud.com>
2024-02-13 17:58:03 +0200

    full interpreter

--- src/compiler/interpreter.py
+++ src/compiler/interpreter.py
@@ -1,43 +1,102 @@
-from typing import Any
-from compiler.parser import BinaryOp, Expression, IfExpression, Literal
+from dataclasses import dataclass
+from typing import Any, Callable
 
-Value = int | bool | None
+from compiler.parser import BinaryOp, Block, Expression, Identifier, IfExpression, Literal, UnaryOp, VariableDeclaration
+
+Value = Callable | int | bool | None
+PredefinedSymbols = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x // y,
+    '<': lambda x, y: x < y,
+    '>': lambda x, y: x > y,
+    '==': lambda x, y: x == y,
+    '<=': lambda x, y: x <= y,
+    '==': lambda x, y: x == y,
+    '==': lambda x, y: x == y,
+    '%': lambda x, y: x % y,
+    'and': lambda x, y: False if not x else x and y,
+    'or': lambda x, y: True if x else y,
+
+    'unary_negative': lambda x: not x if isinstance(x, bool) else -x,
+}
+
+@dataclass
+class SymTab():
+    variables: dict
+
+@dataclass
+class HierarchicalSymTab(SymTab):
+    parent: SymTab
 
 # TODO: add variables, loops, tables, etc...
-def interpret(node: Expression) -> Value:
+def interpret(node: Expression, variables: SymTab = SymTab(variables=PredefinedSymbols)) -> Value:
     match node:
         case Literal():
+            if isinstance(node.value, bool):
+                return node.value == True
             return node.value
         
+        case Identifier():
+            print(variables.variables)
+            if node.name in variables.variables:
+                return variables.variables[node.name]
+            elif isinstance(variables, HierarchicalSymTab):
+                return interpret(node, variables.parent)
+            else:
+                raise Exception(f'Variable {node.name} is not defined')
+        
         case BinaryOp():
-            a: Any = interpret(node.left)
-            b: Any = interpret(node.right)
-            if node.op == '+': # TODO: operators table
-                return a + b
-            elif node.op == '-':
-                return a - b
-            elif node.op == '*':
-                return a * b
-            elif node.op == '/':
-                return a // b
-            elif node.op == '<':
-                return a < b
-            elif node.op == '>':
-                return a > b
+            if isinstance(node.left, Identifier) and node.op == '=':
+                variables.variables[node.left.name] = interpret(node.right, variables)
+                print(variables.variables)
+                return None
+
+            a: Any = interpret(node.left, variables)
+            b: Any = interpret(node.right, variables)
+            topVar = variables
+            while isinstance(topVar, HierarchicalSymTab):
+                topVar = topVar.parent
+            if node.op in topVar.variables:
+                return topVar.variables[node.op](a, b)
             else:
                 raise Exception(f'Unsupported operator "{node.op}"')
             
+        case UnaryOp():
+            x: Any = interpret(node.right, variables)
+            op = 'unary_negative'
+            topVar = variables
+            while isinstance(topVar, HierarchicalSymTab):
+                topVar = topVar.parent
+            if op in topVar.variables:
+                return topVar.variables[op](x)
+            else:
+                raise Exception("Couldn't find unary operator")
+
+            
         case IfExpression():
             if node.else_clause is not None:
-                if interpret(node.cond):
-                    return interpret(node.then_clause)
+                if interpret(node.cond, variables):
+                    return interpret(node.then_clause, variables)
                 else:
-                    return interpret(node.else_clause)
+                    return interpret(node.else_clause, variables)
             else:
-                if interpret(node.cond):
-                    interpret(node.then_clause)
+                if interpret(node.cond, variables):
+                    interpret(node.then_clause, variables)
                 return None
+            
+        case VariableDeclaration():
+            variables.variables[node.name] = interpret(node.initializer, variables)
+            return None
 
+        case Block():
+            childVariables = HierarchicalSymTab({}, variables)
+            for i in range(0, len(node.sequence)-1):
+                interpret(node.sequence[i], childVariables)
+                print(childVariables)
+            return interpret(node.sequence[len(node.sequence)-1], childVariables)
             
         case _:
             raise Exception(f'Unsupported AST node: {node}')
+        
--- tests/interpreter_test.py
+++ tests/interpreter_test.py
@@ -10,6 +10,21 @@ def test_interpreter_works() -> None:
 def test_interpreter_counts() -> None:
     assert interpret(parse(tokenize('1 + 2 * 3'))) == 7
 
+def test_interpreter_unary_minus() -> None:
+    assert interpret(parse(tokenize('-1'))) == -1
+
+def test_interpreter_unary_not() -> None:
+    assert interpret(parse(tokenize('not true'))) == False
+
+def test_interpreter_unary_not_number() -> None:
+    assert interpret(parse(tokenize('not 1'))) == -1
+
+def test_interpreter_counts_bad_expressions() -> None:
+    assert interpret(parse(tokenize('1 + (2 < 3)'))) == 2
+
+def test_interpreter_handles_many_expressions() -> None:
+    assert interpret(parse(tokenize('1 + 2; 2 - 3; 4'))) == 4
+
 def test_interpreter_handles_if_then() -> None:
     assert interpret(parse(tokenize('if 1 < 2 then 3 else 4'))) == 3
 
@@ -18,3 +33,62 @@ def test_interpreter_handles_if_else() -> None:
 
 def test_interpreter_handles_if_in_expression() -> None:
     assert interpret(parse(tokenize('7 + if 1 < 2 then 3 else 4'))) == 10
+
+def test_interpreter_handles_variable_declaration() -> None:
+    assert interpret(parse(tokenize('var a = 1'))) == None
+
+def test_interpreter_handles_variable_declaration_and_change() -> None:
+    assert interpret(parse(tokenize('var a = 1; a = a + 2; a'))) == 3
+
+def test_interpreter_handles_variable_declaration_shadowing_and_change() -> None:
+    assert interpret(parse(tokenize('var a = 1; { var a = 2; a = a + 1; } a = a + 1; a'))) == 2
+
+def test_interpreter_handles_variable_declaration_shadowing_using_itself_and_change() -> None:
+    assert interpret(parse(tokenize('var a = 1; a = { var a = a + 5; a = a + 5; a} a'))) == 11
+
+def test_interpreter_handles_variable_declaration_and_use() -> None:
+    assert interpret(parse(tokenize('var a = 1; a + 2'))) == 3
+
+def test_interpreter_handles_shadow_variable_declaration_and_use() -> None:
+    assert interpret(parse(tokenize('var a = 1; { var a = 2; a + 1 } a + 2'))) == 3
+
+def test_interpreter_handles_block() -> None:
+    assert interpret(parse(tokenize('{1 + 2}'))) == 3
+
+def test_interpreter_handles_block_with_many_expressions() -> None:
+    assert interpret(parse(tokenize('{1 + 2; 2 - 3; 4}'))) == 4
+
+def test_interpreter_handles_block_with_unit_return() -> None:
+    assert interpret(parse(tokenize('{1 + 2; 2 - 3; 4;}'))) == None
+
+def test_interpreter_handles_and_true() -> None:
+    assert interpret(parse(tokenize('true and true'))) == True
+
+def test_interpreter_handles_and_false() -> None:
+    assert interpret(parse(tokenize('false and true'))) == False
+
+def test_interpreter_handles_or_true() -> None:
+    assert interpret(parse(tokenize('true or false'))) == True
+
+def test_interpreter_handles_or_false() -> None:
+    assert interpret(parse(tokenize('false or false'))) == False
+    
+def test_interpreter_handles_or_expression_from_example() -> None:
+    assert interpret(parse(tokenize(
+        """ var evaluated_right_hand_side = false;
+            true or { evaluated_right_hand_side = true; true };
+            evaluated_right_hand_side  // Should be false
+        """
+    ))) == False
+
+def test_interpreter_fails() -> None:
+    assert_interpreter_fails('var a = 1; {var a = 2; var b = 3; a} b')
+
+def assert_interpreter_fails(code: str) -> None:
+    node = parse(tokenize(code))
+    failed = False
+    try:
+        interpret(node)
+    except Exception: # TODO: Exception types
+        failed = True
+    assert failed, f'Parsing succeeded for: {code}'
