7f3374f41639c16119b97ee5a97db722ccdfb1cb
olegTervo <olegtervo@icloud.com>
2024-02-23 19:10:24 +0200

    full type checker for this stage

--- src/compiler/type_checker.py
+++ src/compiler/type_checker.py
@@ -1,6 +1,6 @@
 
 from dataclasses import dataclass
-from compiler.parser import BinaryOp, Expression, IfExpression, Literal
+from compiler.parser import BinaryOp, Block, Expression, Function, Identifier, IfExpression, Literal, UnaryOp, VariableDeclaration, WhileExpression
 
 @dataclass(frozen=True)
 class Type:
@@ -10,46 +10,121 @@ class Type:
 class BasicType(Type):
     name: str
 
+@dataclass
+class SymTab():
+    variables: dict
+
+@dataclass
+class HierarchicalSymTab(SymTab):
+    parent: SymTab
+
 Int = BasicType('Int')
 Bool = BasicType('Bool')
 Unit = BasicType('Unit')
+FunType = BasicType('Function')
 
 # TODO: own exception types
 # TODO: table to track types of variables
 # TODO: add a Type to each AST node
-def typecheck(node: Expression) -> Type:
+def typecheck(node: Expression, variables: SymTab = SymTab({})) -> Type:
     match node:
         case Literal():
-            if isinstance(node.value, int):
+            if isinstance(node.value, bool):
+                return Bool
+            elif isinstance(node.value, int):
                 return Int
+            elif node.value is None:
+                return Unit
             else:
                 raise Exception(f"Don't know the type of literal: {node.value}")
             
+        case Identifier():
+            if node.name in variables.variables:
+                return variables.variables[node.name]
+            elif isinstance(variables, HierarchicalSymTab):
+                return typecheck(node, variables.parent)
+            else:
+                raise Exception(f"Variable {node.name} is not defined")
+            
         case BinaryOp():
-            t1 = typecheck(node.left)
-            t2 = typecheck(node.right)
-            if node.op in ['+', '-', '*', '/']:
+            if isinstance(node.left, Identifier) and node.op == '=':
+                if isinstance(variables.variables, dict) and node.left.name in variables.variables:
+                    variables.variables[node.left.name] = typecheck(node.right, variables)
+                    return Unit
+                elif isinstance(variables, HierarchicalSymTab):
+                    return typecheck(node, variables.parent)
+                else:
+                    raise Exception(f'Asserting unknown variable {node.left.name}')
+
+            t1 = typecheck(node.left, variables)
+            t2 = typecheck(node.right, variables)
+            if node.op in ['+', '-', '*', '/', '%']:
                 if t1 is not Int or t2 is not Int:
                     raise Exception(f'Operator {node.op} expects two Ints, got {t1} and {t2}')
                 return Int
-            elif node.op in ['<', '>']:
+            elif node.op in ['<', '>', '>=', '<=']:
                 if t1 is not Int or t2 is not Int:
                     raise Exception(f'Operator {node.op} expects two Ints, got {t1} and {t2}')
+                return Bool
+            elif node.op in ['==', '!=']:
+                if t1 is not t2:
+                    raise Exception(f'Operator {node.op} expects the same types, got {t1} and {t2}')
+                return Bool
+            elif node.op in ['and', 'or']:
+                if t1 is not Bool or t2 is not Bool:
+                    raise Exception(f'Operator {node.op} expects two Booleans, got {t1} and {t2}')
                 return Bool
             else:
                 raise Exception(f'Unknown operator: {node.op}')
             
+        case UnaryOp():
+            t1 = typecheck(node.right, variables)
+            if node.op == '-':
+                if t1 is not Int:
+                    raise Exception(f'Operator {node.op} expects Int')
+                return Int
+            elif node.op == 'not':
+                if t1 is not Bool:
+                    raise Exception(f'Operator {node.op} expects Bool')
+                return Bool
+            else:
+                raise Exception(f'Unknown unary operator: {node.op}')
+            
         case IfExpression():
-            t1 = typecheck(node.cond)
+            t1 = typecheck(node.cond, variables)
             if t1 is not Bool:
                 raise Exception(f"'if' condition was {t1}")
-            t2 = typecheck(node.then_clause)
+            t2 = typecheck(node.then_clause, variables)
             if node.else_clause is None:
                 return Unit
-            t3 = typecheck(node.else_clause)
+            t3 = typecheck(node.else_clause, variables)
             if t2 != t3:
                 raise Exception(f"'then' and 'else' had different types: {t2} and {t3}")
             return t2
-            
+        
+        case VariableDeclaration():
+            variables.variables[node.name] = typecheck(node.initializer, variables)
+            return Unit
+        
+        case WhileExpression():
+            t1 = typecheck(node.cond, variables)
+            if t1 is not Bool:
+                raise Exception(f"'while' condition {node.cond} was {t1}")
+            retval = node.body
+            if isinstance(retval, Block):
+                retval = retval.sequence[len(retval.sequence)-1]
+            t2 = typecheck(retval, variables)
+            if t2 is not Unit:
+                raise Exception(f"'while' loop return value was {t2}")
+            return Unit
+        
+        case Function():
+            raise Exception("Not implemented")
+        
+        case Block():
+            for i in range(0, len(node.sequence)-1):
+                typecheck(node.sequence[i], variables)
+            return typecheck(node.sequence[len(node.sequence)-1], variables)
+        
         case _:
             raise Exception(f'Unsupported AST node: {node}')
--- tests/type_checker_test.py
+++ tests/type_checker_test.py
@@ -7,11 +7,68 @@ from compiler.type_checker import Bool, Int, Unit, typecheck
 def test_type_checker_works() -> None:
     assert typecheck(parse(tokenize('1 + 2'))) == Int
 
+def test_type_checker_binary_op_minus() -> None:
+    assert typecheck(parse(tokenize('1 - 2'))) == Int
+
+def test_type_checker_binary_op_multiply() -> None:
+    assert typecheck(parse(tokenize('1 * 2'))) == Int
+
+def test_type_checker_binary_op_devide() -> None:
+    assert typecheck(parse(tokenize('1 / 2'))) == Int
+
+def test_type_checker_binary_op_modulo() -> None:
+    assert typecheck(parse(tokenize('1 % 2'))) == Int
+
+def test_type_checker_binary_op_less() -> None:
+    assert typecheck(parse(tokenize('1 < 2'))) == Bool
+
+def test_type_checker_binary_op_more() -> None:
+    assert typecheck(parse(tokenize('1 > 2'))) == Bool
+
+def test_type_checker_binary_op_equal() -> None:
+    assert typecheck(parse(tokenize('1 == 2'))) == Bool
+
+def test_type_checker_binary_op_equal_with_bool_exp() -> None:
+    assert typecheck(parse(tokenize('1 < 2 == true'))) == Bool
+
+def test_type_checker_binary_op_not_equal() -> None:
+    assert typecheck(parse(tokenize('1 != 2'))) == Bool
+
+def test_type_checker_binary_op_not_equal_with_bool_exp() -> None:
+    assert typecheck(parse(tokenize('1 >= 2 != true'))) == Bool
+
+def test_type_checker_binary_op_or() -> None:
+    assert typecheck(parse(tokenize('1 != 2 or 2 != 1'))) == Bool
+
+def test_type_checker_binary_op_and() -> None:
+    assert typecheck(parse(tokenize('1 != 2 and 2 != 1'))) == Bool
+
+def test_type_checker_unary_op_minus() -> None:
+    assert typecheck(parse(tokenize('-1'))) == Int
+
+def test_type_checker_unary_op_not() -> None:
+    assert typecheck(parse(tokenize('not true'))) == Bool
+
 def test_type_checker_bool() -> None:
     assert typecheck(parse(tokenize('1 + 2 < 3'))) == Bool
 
 def test_type_checker_unit() -> None:
     assert typecheck(parse(tokenize('if 1 < 2 then 3'))) == Unit
+
+def test_type_checker_var_int() -> None:
+    assert typecheck(parse(tokenize('var a = 1; a'))) == Int
+
+def test_type_checker_var_bool_true() -> None:
+    assert typecheck(parse(tokenize('var a = true; a'))) == Bool
+
+def test_type_checker_var_bool_false() -> None:
+    assert typecheck(parse(tokenize('var a = false; a'))) == Bool
+
+def test_type_checker_var_bool_exp() -> None:
+    assert typecheck(parse(tokenize('var a = 1 <= 2; a'))) == Bool
+
+def test_type_checker_var_unit() -> None:
+    assert typecheck(parse(tokenize('var a = {1+2;}; a'))) == Unit
 
 def test_type_checker_if_returns_int() -> None:
     assert typecheck(parse(tokenize('if 1 < 2 then 3 else 4'))) == Int
@@ -19,11 +76,44 @@ def test_type_checker_if_returns_int() -> None:
 def test_type_checker_if_returns_bool() -> None:
     assert typecheck(parse(tokenize('if 1 < 2 then 3 < 4 else 4 < 5'))) == Bool
 
+def test_type_checker_while_returns_unit() -> None:
+    assert typecheck(parse(tokenize('while true do 3;'))) == Unit
+
+def test_type_checker_block_unit_return_from_block() -> None:
+    assert typecheck(parse(tokenize('var a = 1; {a = a+1; a;}'))) == Unit
+
+def test_type_checker_block_unit_return_semicolon_after_block() -> None:
+    assert typecheck(parse(tokenize('var a = 1; {a = a+1; a};'))) == Unit
+
+def test_type_checker_block_unit_return_after_block() -> None:
+    assert typecheck(parse(tokenize('var a = 1; {a = a+1; a} a;'))) == Unit
+
+def test_type_checker_block_int_return_from_block() -> None:
+    assert typecheck(parse(tokenize('var a = 1; {a = a+1; a}'))) == Int
+
+def test_type_checker_block_int_return() -> None:
+    assert typecheck(parse(tokenize('var a = 1; {a = a+1; a} a'))) == Int
+
 def test_type_check_can_fail() -> None:
     assert_type_checker_fails('(1 < 2) + 3')
 
 def test_type_check_fails_if_condition_not_bool() -> None:
     assert_type_checker_fails('if 1 then 3 else 4 < 5')
+
+def test_type_check_fails_binary_operations_expected_int() -> None:
+    assert_type_checker_fails('1 + true')
+    assert_type_checker_fails('1 - true')
+    assert_type_checker_fails('1 * true')
+    assert_type_checker_fails('1 / true')
+    assert_type_checker_fails('1 % true')
+
+def test_type_check_fails_binary_operations_expected_bool() -> None:
+    assert_type_checker_fails('1 and true')
+    assert_type_checker_fails('true or 1')
+
+def test_type_check_fails_unary_operations() -> None:
+    assert_type_checker_fails('not 1')
+    assert_type_checker_fails('-false')
 
 def test_type_check_fails_different_if_return_types() -> None:
     assert_type_checker_fails('if 1 < 2 then 3 else 4 < 5')
